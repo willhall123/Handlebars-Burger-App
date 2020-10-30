828e7ec2846d041f72503845c897f0a0263e76b3
William Hall <willhall123@icloud.com>
2020-10-29 22:04:09 -0500

    inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use bugers_db;
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name) 
+VALUES ('Smokehouse Burger');

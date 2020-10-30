c7c630d169366a41515a9939643a93d07a25419b
William Hall <willhall123@icloud.com>
2020-10-30 08:58:19 -0500

    edits

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,11 @@
 var express = require("express");
-var burger = require("../config/burger.js");
+var burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     res.render("index", hbsObject);
   });

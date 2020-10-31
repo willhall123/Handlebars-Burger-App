a23623414f1900a9599ce409f22985640deef8ef
William Hall <willhall123@icloud.com>
2020-10-31 05:21:43 -0500

    more debugging

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }  
 else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
